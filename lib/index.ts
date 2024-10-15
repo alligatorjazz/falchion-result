4f4e97253acf56d855a269ebb7d8095b0dd53cb5
Damian Thomas <dimitrisonic@gmail.com>
2024-10-15 10:52:35 -0400

    added ok() and notOk() functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,15 @@
+// import { Result } from "./types.js";
+
 export * from "./types.js";
+export function ok<T>(successData: T): { ok: true } & T {
+	return { ...successData, ok: true };
+}
+
+export function notOk<T>(failureData: T): { ok: false } & T {
+	return { ...failureData, ok: false };
+}
+
+// const x = ok({ status: "good" });
+// function test(): Result<{ data: string }> {
+// 	return ok({ data: "hello world" });
+// }
