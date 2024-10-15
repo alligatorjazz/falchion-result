bcedca347912a071c3f4dd3c5ff498fd30eb9c1e
Damian Thomas <dimitrisonic@gmail.com>
2024-10-15 10:55:49 -0400

    updated package.lock to reflect new package name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,7 @@
 export * from "./types.js";
+export declare function ok<T>(successData: T): {
+    ok: true;
+} & T;
+export declare function notOk<T>(failureData: T): {
+    ok: false;
+} & T;
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,12 @@
+// import { Result } from "./types.js";
 export * from "./types.js";
+export function ok(successData) {
+    return { ...successData, ok: true };
+}
+export function notOk(failureData) {
+    return { ...failureData, ok: false };
+}
+// const x = ok({ status: "good" });
+// function test(): Result<{ data: string }> {
+// 	return ok({ data: "hello world" });
+// }
